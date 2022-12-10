6de8cc35041e6768e1d586e99afe7f245c9b514d
eliphrock <eliphrock@gmail.com>
2022-12-10 13:38:50 -0500

    selenium

---
+++ src/test/java/FirstClass.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstClass {
+    public static void main(String[] args) {
+
+       // set uo chrome driver
+        System.setProperty("webdriver.chrome.driver","./src/recources/drivers/chromedriver");
+
+       // create chrome driver
+
+        WebDriver driver=new ChromeDriver();
+
+        // created webdriver ,we can automate web application
+
+        driver.get("https://www.techproeducation.com");
+    }
+
+}
