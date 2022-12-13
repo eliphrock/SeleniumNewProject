76dae0649452e4f527c8c62553b01e19deca27bd
eliphrock <eliphrock@gmail.com>
2022-12-12 21:36:45 -0500

    new commit

--- src/test/java/tests/Day03_Locaters.java
+++ src/test/java/tests/Day03_Locaters.java
@@ -1,6 +1,8 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
@@ -12,40 +14,44 @@ import java.time.Duration;
 
 public class Day03_Locaters {
 
-    /*
-        Create a class : LocatorsIntro 
-        Create test method locators
-
-
-        Enter below username and password then click sign in button 
-        Username :  testtechproed@gmail.com 
-        Password : Test1234! 
-        Then verify that the expected user id  testtechproed@gmail.com (USE getText() method to get the text from the page)
-        Verify the Addresses and Sign Out  texts are displayed
-        Find the number of total link on the page
-        Sign out from the page
-     */
+    WebDriver driver;
     @Before
-    @Test
-    public void logIn(){
-
-        //user goes to http://a.testaddressbook.com/sign_in
+    public void setUp(){
+//        user goes to http://a.testaddressbook.com/sign_in
         WebDriverManager.chromedriver().setup();
-        WebDriver driver=new ChromeDriver();
-        //implicit wait
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver = new ChromeDriver();
+//        IMPLICIT WAIT
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));//15 seconds wait in case needed
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
-      //Locate the elements of email textbox, password textbox, and signin button
-        WebElement username=driver.findElement(By.name("username"));//location username by name
-        username.sendKeys("Admin");//typing Admin
-        //locating the password and typing
-        WebElement password=driver.findElement(By.name("password"));
+    }
+    @Test
+    public void logInTest(){
+//        Locating the username and typing
+        WebElement username = driver.findElement(By.name("username"));//Location username bu name
+        username.sendKeys("Admin");// typing Admin
+//        Locating the password and typing
+        WebElement password = driver.findElement(By.name("password"));
         password.sendKeys("admin123");
-        //locating the button and clicking
-        WebElement button=driver.findElement(By.tagName("button"));
+//        Locating the button and clicking
+        WebElement button = driver.findElement(By.tagName("button"));
         button.click();
+//        Verifying the log in is successful
+//        1. We can use default page URL to do assertion
+//        2. Or we can locate a CORE ELEMENT on the default page to assert is that element is displayed on the page
+//        Login Page : https://opensource-demo.orangehrmlive.com/web/index.php/auth/login
+//        Default Page : https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index
+        String actualURL = driver.getCurrentUrl();
+        String expectedURL = "https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index";
+        Assert.assertEquals("LOGIN FAILED",expectedURL,actualURL);//If login successful PASS, if not FAIL and print message LOGIN FAILED"
+//      NOTE : We do not need to use containers. We can use method chain in selenium
+//        driver.findElement(By.name("username")).sendKeys("Admin");
+//        driver.findElement(By.name("password")).sendKeys("admin123");
+//        driver.findElement(By.tagName("button")).click();
+    }
 
-
+    @After
+    public void tearDown(){
+        driver.quit();
     }
 }
