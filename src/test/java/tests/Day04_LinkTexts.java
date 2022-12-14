a6791ede0ec0cc3b04b40856b430fc36a597057c
eliphrock <eliphrock@gmail.com>
2022-12-13 20:12:32 -0500

    new commit

--- src/test/java/tests/Day04_LinkTexts.java
+++ src/test/java/tests/Day04_LinkTexts.java
@@ -1,10 +1,13 @@
 package tests;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.time.Duration;
@@ -18,22 +21,33 @@ public class Day04_LinkTexts {
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
         driver.manage().window().maximize();
     }
+    /*
+    linktext and partialLinkText are case sensitive
+     */
     @Test
     public void LMSPage(){
         driver.get("https://techproeducation.com/");
         //Click on LMS LOGIN
-        //lms login is a link
-        //text of that link is "lms login"
+        //LMS LOGIN IS A LINK
+        //TEXT OF THAT LINK IS "LMS LOGIN"
         driver.findElement(By.linkText("LMS LOGIN")).click();
-
+        //Verify lms page is visible
+        String expectedURL ="https://lms.techproeducation.com/";
+        String actualURL=driver.getCurrentUrl();
+        Assert.assertEquals("LMS LOGIN PAGE IS NOT DISPLAYED",expectedURL,actualURL);
     }
     @Test
-    public void LMSPagePartial(){
+    public void LMSPagePartialLinkText(){
         driver.get("https://techproeducation.com/");
-        //Click on LMS LOGIN
-        //lms login is a link
-        //text of that link is "lms login"
-        driver.findElement(By.partialLinkText("LMS L")).click();
-
+        driver.findElement(By.partialLinkText("LMS LO")).click();
+        //ALTERNATIVELY WE CAN LOCATE A CORE ELEMENT ON LMS PAGE AND CHECK IF THAT ELEMENT IS DISPLAYED IN THE PAGE
+        WebElement loginElement = driver.findElement(By.linkText("Login/Register"));
+        Assert.assertTrue(loginElement.isDisplayed());//isDisplayed() returns TRUE is element is on the page. returns false if element is not displayed on the page
+        //Assert.assertTrue(driver.findElement(By.linkText("Login/Register")).isDisplayed());
     }
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+
 }
