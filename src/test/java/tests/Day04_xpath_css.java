7fdc4421d727c2c34303f9d4c4e09a880b6f50fa
eliphrock <eliphrock@gmail.com>
2022-12-13 21:50:05 -0500

    css

---
+++ src/test/java/tests/Day04_xpath_css.java
@@ -0,0 +1,48 @@
+package tests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Day04_xpath_css {
+
+    WebDriver driver;
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver=new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.manage().window().maximize();
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+    }
+    @Test
+    public void loginTest(){
+        driver.findElement(By.xpath("//input[@name='username']")).sendKeys("Admin");
+        driver.findElement(By.xpath("//input[@name='password']")).sendKeys("admin123");
+        driver.findElement(By.xpath("//button[@type='submit']")).click();
+    }
+
+    @Test
+    public void loginTestCss(){
+        driver.findElement(By.cssSelector("input[name='username']")).sendKeys("Admin");
+        driver.findElement(By.cssSelector("input[name='password']")).sendKeys("admin123");
+        driver.findElement(By.cssSelector("button[type='submit']")).click();
+    }
+    @After
+    public void tearDown(){
+        driver.quit();
+    }
+    /*
+     * How many locators are there?
+     * What are they?
+     * Which one do you prefer? Why?
+     * What is difference between absolute xpath and relative xpath?
+     * What is the difference between xpath and css selector?
+     */
+}
