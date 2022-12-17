b5845d631a6f9c325e41b88ac4342f2d5296f67d
eliphrock <eliphrock@gmail.com>
2022-12-17 14:14:10 -0500

    new

--- src/test/java/tests/Day05_DropDown.java
+++ src/test/java/tests/Day05_DropDown.java
@@ -9,10 +9,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-
 import java.time.Duration;
 import java.util.List;
-
 public class Day05_DropDown {
     WebDriver driver;
     @Before
@@ -67,9 +65,9 @@ public class Day05_DropDown {
         }
 //    5. Verify the dropdown has Option 2 text
         boolean isOption2Exist=false;
-        for (WebElement eachOption : allOptions){
-            if (eachOption.getText().equals("gadhstdhdrtyjh")){
-                isOption2Exist=true;
+        for (WebElement eachOption : allOptions) {
+            if (eachOption.getText().equals("Option 2")) {
+                isOption2Exist = true;
             }
         }
 //        fails if Option 2 is not in the dropdown
@@ -77,17 +75,34 @@ public class Day05_DropDown {
         Assert.assertTrue(isOption2Exist);
     }
     //6.Create method printFirstSelectedOptionTest Print first selected option
-
     @Test
     public void printFirstSelectedOptionTest(){
         WebElement dropdown = driver.findElement(By.id("dropdown"));
         Select select = new Select(dropdown);
-        System.out.println("Selected Option :" + select.getFirstSelectedOption().getText());
+//        printing the selected option
+        System.out.println("Selected Option : "+select.getFirstSelectedOption().getText());
+//        selecting a different option from the dropdown
+        select.selectByIndex(2);
+//        printing the selected option
+        System.out.println("Selected Option : "+select.getFirstSelectedOption().getText());
+//        7.Find the size of the dropdown,
+//        Print "Expected Is Not Equal Actual" if there are not 3 elements in the dropdown.
+        int numberOfOptions = select.getOptions().size();
+        int expectedNumberOfOptions = 3;
+//        Assertion will print Expected Is Not Equal Actual ONLY IF THIS ASSERTION FAILS
+        Assert.assertEquals("Expected Is Not Equal Actual",expectedNumberOfOptions,numberOfOptions);
     }
     @After
     public void tearDown(){
         driver.quit();
     }
-
-//7.Find the size of the dropdown, Print "Expected Is Not Equal Actual" if there are not 3 elements in the dropdown.
+    /*
+    -What is a dropdown?
+        ---Dropdown is a webelement. It has list of options.
+    -How do you handle dropdown? What do you do to automate dropdown?
+        ---Dropdowns are created using Select class. So we first locate the dropdown element, then use select class
+        to interact with dropdown elements.
+    -How do you select dropdown elements?
+        ---We actually have 3 options to select a dropdown(index,value,visible text)
+     */
 }
