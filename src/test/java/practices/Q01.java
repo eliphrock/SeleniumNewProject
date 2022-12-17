3dad3926d933f54a2648262957b6fd31e3b77c7c
eliphrock <eliphrock@gmail.com>
2022-12-17 10:33:00 -0500

    practice

---
+++ src/test/java/practices/Q01.java
@@ -0,0 +1,44 @@
+package practices;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Q01 {
+
+    public static void main(String[] args) throws InterruptedException {
+        // Set Driver Path
+        System.setProperty("webdriver.chrome.driver","src/recources/drivers/chromedriver");
+
+        // Create chrome driver object
+        WebDriver driver =  new ChromeDriver();
+
+        // Maximize the window
+        driver.manage().window().maximize();
+
+        // Open google home page https://www.google.com/
+        driver.get("https://www.google.com/");
+
+        // Navigate to techproeducation home page https://techproeducation.com/
+        driver.navigate().to("https://techproeducation.com/");
+
+        // Navigate back to google
+        Thread.sleep(1000);
+        driver.navigate().back();
+
+        // Navigate forward to techproeducation
+        Thread.sleep(1000);
+        driver.navigate().forward();
+
+        // Refresh the page
+        driver.navigate().refresh();
+
+        // Close/Quit the browser
+        driver.quit();
+
+        // And last step : print "ALL OK" on console
+        System.out.println("ALL OK");
+
+
+
+    }
+}
