e8648f32b46f24d3296ae2aff456e6fca321905b
eliphrock <eliphrock@gmail.com>
2022-12-23 21:46:55 -0500

    google

--- src/test/java/homeworks/GoogleSearchTest.java
+++ src/test/java/homeworks/GoogleSearchTest.java
@@ -1,5 +1,6 @@
 package homeworks;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
@@ -19,6 +20,37 @@ public class GoogleSearchTest extends TestBase {
        // And print how many related results displayed on Google
        WebElement result=driver.findElement(By.id("result-stats"));
         System.out.println(result.getText());
+
+        driver.quit();
     }
+
+@Test
+    public void titleTest(){
+       //titleTest =>Verify if google title = “Google” 
+   driver.get("https://www.google.com/");
+   Assert.assertEquals("Google",driver.getTitle());
+   driver.quit();
+
+}
+@Test
+    public void imageTest(){
+
+    //    imageTest => Verify if google image displays or not 
+    driver.get("https://www.google.com/");
+    WebElement img=driver.findElement(By.id("hplogo"));
+    Assert.assertTrue(img.isDisplayed()); ;
+    driver.quit();
+
+}
+@Test
+    public void gmailLinkTest(){
+    //    gmailLinkTest => Verify if the Gmail link is displayed or not
+    driver.get("https://www.google.com/");
+    WebElement link=driver.findElement(By.xpath("//a[@class='gb_j']"));
+    Assert.assertTrue(link.isDisplayed()); ;
+    driver.quit();
+
+
+}
 
 }
