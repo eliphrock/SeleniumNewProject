59d18989cdb0c831fc9fef9ee3e0d0fffe433680
eliphrock <eliphrock@gmail.com>
2022-12-24 12:11:21 -0500

    autocomplete

---
+++ src/test/java/tests/Day10_AutoComplete.java
@@ -0,0 +1,42 @@
+package tests;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import utilities.TestBase;
+
+public class Day10_AutoComplete extends TestBase {
+    @Test
+    public void autoCompleteTest(){
+
+        driver.get("https://testcenter.techproeducation.com/index.php?page=autocomplete");
+        //when user type "uni"
+
+        driver.findElement(By.id("myCountry")).sendKeys("uni");
+
+        //and select the United Kingdom from the suggestions
+        driver.findElement(By.xpath("//div[@id='myCountryautocomplete-list']//div[.='United Kingdom']")).click();
+
+        //And click on submit button
+        driver.findElement(By.xpath("//input[@type='button']")).click();
+
+        //Then verify the result contains "United Kingdom"
+        Assert.assertTrue(driver.findElement(By.id("result")).getText().contains("United Kingdom"));
+
+
+
+    }
+
+
+    @Test
+    public void autoCompleteTestWithReusableMethod() throws InterruptedException {
+
+        driver.get("https://testcenter.techproeducation.com/index.php?page=autocomplete");
+        searchAndSelectFromList("uni", "United Kingdom");
+
+        searchAndSelectFromList("an", "Angola");
+
+        searchAndSelectFromList("bah", "Bahamas");
+
+    }
+}
--- src/test/java/tests/Day10_FileUploadTest.java
+++ src/test/java/tests/Day10_FileUploadTest.java
@@ -17,10 +17,10 @@ public class Day10_FileUploadTest extends TestBase {
         Thread.sleep(2000);
 
 //        2. select a file from your computer
-        String userHOME=System.getProperty("user.home");
+        String userHOME = System.getProperty("user.home");
         String pathOfFile = userHOME + "/Desktop/logo.jpeg";
 
-      //sending the path of the file that I want to upload
+        //sending the path of the file that I want to upload
         //we can use sendkeys cause the input type file combination
         chooseAFileButton.sendKeys(pathOfFile);
 
@@ -33,11 +33,17 @@ public class Day10_FileUploadTest extends TestBase {
 
         //4 asserting
 
-        boolean isEqual=driver.findElement(By.xpath("//h3")).getText().equals("File Uploaded!");
+        boolean isEqual = driver.findElement(By.xpath("//h3")).getText().equals("File Uploaded!");
         Assert.assertTrue(isEqual);
     }
-//    When user goes to https://the-internet.herokuapp.com/upload
-//    When user selects an image from the desktop
-//    And click on the upload button
-//    Then verify the File Uploaded!  Message displayed
 }
+//  how do you upload a file with selenium?
+//first selenium has limitation automation of files.But file upload can be done with selenium.
+//we locate the choose file button and use sendkeys function to send the path of file.
+//then we click upload button to upload the file.
+//this will automaticlly upload the file
+
+//LIMITATION
+//selenium has limitation automation desktop apps
+//For example, we can not find path of files, or verify if file exists on out machines with selenum
+//Instead we use java File libraries
